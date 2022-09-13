3e9a801574995f77ed023270e191906e6c8b1854
Reuben Joseph <reuben.joseph@soti.net>
2022-09-13 18:27:01 +0530

    All changes were commited

---
+++ Soti.Training.Batch2.CollectionTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Soti.Training.Batch2.Collections;
+
+namespace Soti.Training.Batch2.CollectionTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CheckArrayList()
+        {
+            
+        }
+    }
+}
---
+++ Soti.Training.Batch2.Collections/ArrList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soti.Training.Batch2.Collections
+{
+    public class ArrList
+    {
+        /// <summary>
+        /// Different Array List fucntions are 
+        /// </summary>
+        /// <param name="args"></param>
+        public static void ArrayList1()
+        {
+            ArrayList myAL = new ArrayList();
+            myAL.Add("Reuben");
+            myAL.Add("Joseph");
+            myAL.Add("Philip");
+            foreach (Object obj in myAL)
+                Console.Write("   {0}", obj);
+            Console.WriteLine();
+        }
+        public static void Main(string[] args)
+        {
+            ArrayList1();
+        }
+    }
+}
