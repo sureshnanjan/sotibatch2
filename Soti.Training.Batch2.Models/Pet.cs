646d5afd346c747a1add011a2370b33034c54bb3
Hiba Muhamed Zaid <hibamohammedzaid@gmail.com>
2022-09-13 14:08:39 +0530

    This is the modification on my branch

--- Soti.Training.Batch2.Models/Pet.cs
+++ Soti.Training.Batch2.Models/Pet.cs
@@ -8,6 +8,7 @@ namespace Soti.Training.Batch2.Models
 {
     /// <summary>
     /// This is the Type for represting pets in the Petstore
+    /// This is the change to be reflected in the branch
     /// </summary>
     public class Pet // This Name of the Clss
     {
