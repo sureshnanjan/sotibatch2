107ab437faf966119b732d61891379835327ff27
Dennis Vadakkedam <dennis.vadakkedam@soti.net>
2022-09-13 21:47:53 +0530

    changes were made

---
+++ Soti.Training.Batch2/ArrList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soti.Training.Batch2
+{
+    public class ArrList
+    {
+        public void list()
+        {
+            ArrayList myAL = new ArrayList();
+            myAL.Add("Dennis");
+            myAL.Add("Vadakkedam");
+            myAL.Add("SOTI");
+            myAL.AddRange(new ArrayList() { "range", "array" });
+            foreach (Object obj in myAL)
+                Console.Write("   {0}", obj);
+            Console.WriteLine();
+
+
+        }
+        
+
+    }
+}
