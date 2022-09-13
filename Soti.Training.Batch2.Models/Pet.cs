924774cf60ac7d34399c2c8a139072e016921c7c
Antony K Sebastian <antony.sebastian@soti.net>
2022-09-13 13:48:41 +0530

    modified by Antony

--- Soti.Training.Batch2.Models/Pet.cs
+++ Soti.Training.Batch2.Models/Pet.cs
@@ -7,7 +7,8 @@ using System.Threading.Tasks;
 namespace Soti.Training.Batch2.Models
 {
     /// <summary>
-    /// This is the Type for represting pets in the Petstore
+    /// This is the Type for represting pets in the Petstore,
+    /// adding this comment for git demo
     /// </summary>
     public class Pet // This Name of the Clss
     {
