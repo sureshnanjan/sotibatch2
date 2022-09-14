b183cd879da36d2549c75c3dc7c8246b5e629db8
Dennis Vadakkedam <dennis.vadakkedam@soti.net>
2022-09-14 11:55:12 +0530

    hashtable dispaly

---
+++ Soti.Training.Batch2/Hash.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soti.Training.Batch2
+{
+    public class Hash
+    {
+        public void hashlist()
+        {
+            Hashtable myhash = new Hashtable()
+            {
+                { 1,"dennis"},
+                { 2,"deepak"},
+                { 3,"alex"}
+            };
+            myhash.Add(5, "hello");
+            myhash.Add(4, "world");
+            foreach(DictionaryEntry value in myhash)
+            {
+                Console.WriteLine($"key value {value.Key} value of element {value.Value}");
+            }
+            if (myhash.ContainsKey(1))
+            {
+                myhash.Remove(1);
+            }
+            Console.WriteLine("After removal");
+            foreach (DictionaryEntry value in myhash)
+            {
+                Console.WriteLine($"key value {value.Key} value of element {value.Value}");
+            }
+
+
+
+        }
+      
+    }
+}
---
+++ Soti.Training.Batch2/Stacking.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soti.Training.Batch2
+{
+    public class Stacking
+    {
+        public void stack1()
+        {
+
+
+            Stacking myStack = new Stacking();
+        //    myStack.Push("Hello");
+         //   myStack.Push("World");
+         //   myStack.Push("!");
+        }
+
+    }
+}
