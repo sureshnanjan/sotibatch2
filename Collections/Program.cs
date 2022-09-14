5e8726e8ba18819d866c7c0d7110827b3f8c757b
anitzz <anitz04.ast@gmail.com>
2022-09-14 08:54:16 +0530

    Committed by Anitta

---
+++ Collections/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Collections
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           
+            //Here ArrayList is inbuilt class
+            ArrayList myAl = new ArrayList();
+            myAl.Add("Hello");
+            myAl.Add("World");
+            myAl.Add("!");
+
+            //To print values of ArrayList myAl
+            Console.WriteLine("Values:");
+            foreach (var item in myAl)
+            {
+                Console.WriteLine("{0}",item);
+            }
+
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Count: {0}", myAl.Count);
+            Console.WriteLine("Capacity: {0}", myAl.Capacity);
+            Console.ReadKey();
+
+        }
+    }
+}
