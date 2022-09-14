3ef08d55913d574ccb1821430d5ee1d7e10f6f80
Simna Ashraf <simnaashraf7@gmail.com>
2022-09-14 12:45:15 +0530

    system collection

---
+++ sotibatch2.SystemCollectionsTests/QueueCollections.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
+
+namespace sotibatch2.SystemCollectionsTests
+{
+    [TestClass]
+    public class QueueCollections
+    {
+        [TestMethod]
+        public void Queue()
+        {
+            Queue callerIds = new Queue();
+            callerIds.Enqueue(1);
+            callerIds.Enqueue(2);
+            callerIds.Enqueue(3);
+            callerIds.Enqueue(4);
+
+            foreach (var id in callerIds)
+                Console.Write(id);
+            Console.WriteLine("\n");
+            Console.WriteLine(callerIds.Dequeue());
+
+            foreach (var id in callerIds)
+                Console.Write(id);
+
+        }
+    }
+}
---
+++ sotibatch2.SystemCollectionsTests/StackCollections.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
+
+namespace sotibatch2.SystemCollectionsTests
+{
+    [TestClass]
+    public class StackCollections
+    {
+        [TestMethod]
+        public void stack()
+        {
+            Stack myStack = new Stack();
+            myStack.Push("Hello");
+            myStack.Push("World");
+            myStack.Push("!");
+
+            foreach (var item in myStack)
+                Console.Write(item + ",");
+                Console.Write("\n");
+            myStack.Pop();
+
+            foreach (var item in myStack)
+                Console.Write(item + ",");
+
+
+        }
+    }
+}
