84672e45dfdc229b188511306f22c7df475c030d
Gokul Ps <gokul.ps@soti.net>
2022-09-16 20:08:59 +0530

    Changes made by Gokul

---
+++ Soti.Training.Batch2/SQLLite.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Microsoft.Data.Sqlite;
+using Soti.Training.Batch2.Models;
+using System.Configuration;
+
+
+
+namespace Soti.Training.Batch2
+{
+    public class SQLLiteDemo
+    {
+
+        public static void ShowConnectionStrings()
+        {
+            ConnectionStringSettingsCollection settings =
+                    ConfigurationManager.ConnectionStrings;
+
+
+
+            if (settings != null)
+            {
+                foreach (ConnectionStringSettings cs in settings)
+                {
+                    Console.WriteLine(cs.Name);
+                    Console.WriteLine(cs.ProviderName);
+                    Console.WriteLine(cs.ConnectionString);
+                }
+            }
+            Console.WriteLine("The Appsettings are for the environment");
+            foreach (var item in ConfigurationManager.AppSettings)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+        }
+
+
+
+
+
+    }
+}
